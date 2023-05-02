7d0183b2f2592c4248de8ed798e015bfafbd4483
lucerogiron <gironlucero98@gmail.com>
2023-05-02 10:24:07 -0700

    just some writing

--- portal-app/src/App.jsx
+++ portal-app/src/App.jsx
@@ -9,10 +9,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -20,7 +20,7 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and Luci is just waaaaaaay cooler than Josh
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
