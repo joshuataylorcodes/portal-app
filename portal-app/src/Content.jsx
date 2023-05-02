e2d798d022f3dad30591b8e70e97f2ebbb1e6488
“lukenilsson” <luke.nilsson@gmail.com>
2023-05-02 16:51:05 -0400

    displaying resume data

--- portal-app/src/Content.jsx
+++ portal-app/src/Content.jsx
@@ -1,15 +1,27 @@
 import { LogoutLink } from "./LogoutLink";
 import { Axios } from "axios";
+import { ResumeShow } from "./ResumeShow";
 
 export function Content() {
-  const resumes = [
-    { id: 1, name: "Leah Perri", email: "leah@example.com" },
-    { id: 2, name: "Luke Nilsson", email: "luke@example.com" },
+  const resume = [
+    {
+      id: 1,
+      first_name: "Leah ",
+      last_name: "Perri",
+      email: "leah@example.com",
+      phone_number: "123.456.7890",
+      bio: "I'm super kewl.",
+      linkedin_url: "linkedin.com/leah",
+      twitter_url: "twitter.com/leah",
+      website: "leah.com",
+      resume_url: "leahresume.googledocs.com",
+      github: "github.com/leah",
+      photo: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
+    },
   ];
   return (
     <div>
-      <h1>My Resume</h1>
-      <ResumeShow resumes={resumes} />
+      <ResumeShow resume={resume} />
     </div>
   );
 }
--- portal-app/src/ResumeShow.jsx
+++ portal-app/src/ResumeShow.jsx
@@ -1,21 +1,29 @@
 import { useState } from "react";
 
+const handleResumeShow = () => {
+  console.log("God? Dad?");
+};
 export function ResumeShow(props) {
   return (
     <div id="resume-show">
-      <h1>Update Resume</h1>
-      {props.resumes.map((resume) => (
-        <div key={resume.id}>
-          <form>
-            <h2>
-              Name <input defaultValue={resume.name} type="text" />{" "}
-            </h2>
-            <h2>
-              Email <input defaultValue={resume.email} type="text" />{" "}
-            </h2>
-          </form>
+      <h1>My Resume</h1>
+      <div className="resume-container">
+        <div id="resume-name">
+          {props.first_name}
+          {props.last_name}
         </div>
-      ))}
+        <div id="resume-email">{props.email}</div>
+        <div id="resume-phone">{props.phone_number}</div>
+        <div id="resume-bio">{props.bio}</div>
+        <div id="resume-linkedin">{props.linkedin_url}</div>
+        <div id="resume-twitter">{props.twitter_url}</div>
+        <div id="resume-website">{props.website}</div>
+        <div id="resume-resume-url">{props.resume_url}</div>
+        <div id="resume-github">{props.github}</div>
+        <div id="resume-photo">
+          <img src={props.photo} />
+        </div>
+      </div>
     </div>
   );
 }
