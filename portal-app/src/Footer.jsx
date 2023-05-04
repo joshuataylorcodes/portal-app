22cd650181eab5f524dc789aed92d422dc749b74
“lukenilsson” <luke.nilsson@gmail.com>
2023-05-04 15:06:14 -0400

    login css / misc css

--- portal-app/src/Footer.jsx
+++ portal-app/src/Footer.jsx
@@ -3,7 +3,7 @@ import { LogoutLink } from "./LogoutLink";
 
 export function Footer() {
   return (
-    <div class="footer">
+    <div className="footer">
       <a href="/"> Home |</a>
       <a href="/login"> Login |</a>
       <a href="#"> Logout</a>
--- portal-app/src/Header.jsx
+++ portal-app/src/Header.jsx
@@ -6,24 +6,27 @@ export function Header() {
     <header>
       <nav>
         {localStorage.jwt === undefined ? (
-          <ul>
-            <li>
-              <a href="/login" className="button">
-                Login
-              </a>
-            </li>
-            <li>
-              <a href="/" className="button">
-                Home Page
-              </a>
-            </li>
-            <li>
-              <p className="navmsg">
-                {"                   "}
-                Welcome to your Student Portal! Please Login to View your Resume{" "}
-              </p>
-            </li>
-          </ul>
+          <div className="header">
+            <ul>
+              <li>
+                <a href="/login" className="button">
+                  Login
+                </a>
+              </li>
+              <li>
+                <a href="/" className="button">
+                  Home Page
+                </a>
+              </li>
+              <li>
+                <p className="navmsg">
+                  {"                   "}
+                  Welcome to your Student Portal! Please Login to View your
+                  Resume{" "}
+                </p>
+              </li>
+            </ul>
+          </div>
         ) : (
           <li>
             <a href="/logout" className="button">
